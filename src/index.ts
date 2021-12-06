2c792570afcabe1ac76eae1fa82904dc929e3f34
Hung Nguyen <hunglnl01@gmail.com>
2021-12-05 23:52:40 -0800

    firebase auth for node

--- src/index.ts
+++ src/index.ts
@@ -31,6 +31,8 @@ declare module 'express-session' {
 	}
 }
 
+export var admin = require('firebase-admin');
+
 export const client = new Client({
 	host: 'localhost',
 	user: 'postgres',
@@ -41,6 +43,14 @@ export const client = new Client({
 (async () => {
 	const app = express();
 
+	// firebase authentication
+	var serviceAccount = require('../serviceAccountKey.json');
+
+	admin.initializeApp({
+		credential: admin.credential.cert(serviceAccount),
+	});
+
+	// postgres db
 	client.connect((err: any) => {
 		if (err) {
 			console.error('postgres connection error', err.stack);
@@ -97,35 +107,6 @@ export const client = new Client({
 			saveUninitialized: false,
 		})
 	);
-
-	app.get('/', (req, res) => {
-		const sess = req.session;
-		if (sess.username && sess.password) {
-			if (sess.username) {
-				res.write(`<h1>Welcome ${sess.username} </h1><br>`);
-				res.write(`<h3>This is the Home page</h3>`);
-				res.end('<a href=' + '/logout' + '>Click here to log out</a >');
-			}
-		} else {
-			res.sendFile(__dirname + '/login.html');
-		}
-	});
-	app.post('/login', (req, res) => {
-		const sess = req.session;
-		const { username, password } = req.body;
-		sess.username = username;
-		sess.password = password;
-		// add username and password validation logic here if you want.If user is authenticated send the response as success
-		res.end('success');
-	});
-	app.get('/logout', (req, res) => {
-		req.session.destroy(err => {
-			if (err) {
-				return console.log(err);
-			}
-			res.redirect('/');
-		});
-	});
 
 	app.post('/refresh_token', async (req, res) => {
 		const token = req.cookies.jid;
--- src/utils/authentication/isAuth.ts
+++ src/utils/authentication/isAuth.ts
@@ -1,21 +1,30 @@
-import { verify } from 'jsonwebtoken';
+import { admin } from '../../index';
 import { MiddlewareFn } from 'type-graphql';
 import { MyContext } from '../context/MyContext';
 
-// bearer 1325151
+// Firebase Auth
 export const isAuth: MiddlewareFn<MyContext> = ({ context }, next) => {
 	const authorization = context.req.headers['authorization'];
 
 	if (!authorization) {
 		throw new Error('not authenticated');
 	}
+
+	if (authorization && authorization.split(' ')[0] !== 'Bearer') {
+		throw new Error('invalid token');
+	}
+
 	try {
 		const token = authorization.split(' ')[1];
-		const payload = verify(token, process.env.ACCESS_TOKEN_SECRET!);
-		context.payload = payload as any;
+		admin
+			.auth()
+			.verifyIdToken(token)
+			.then(() => next())
+			.catch(() => {
+				throw new Error('could not authenticate with firebase');
+			});
 	} catch (e) {
-		console.log(e);
-		throw new Error('not authenticated');
+		throw new Error('cannot connect to firebase');
 	}
 
 	return next();
