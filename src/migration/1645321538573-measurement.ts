54b96a2a7c8da212064424927a7c432f0715653c
Hung Nguyen <hunglnl01@gmail.com>
2022-06-17 23:53:46 -0700

    fix deployment issue

--- src/migration/1645321538573-measurement.ts
+++ src/migration/1645321538573-measurement.ts
@@ -3,28 +3,18 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class measurement1645321538573 implements MigrationInterface {
 	public async up(queryRunner: QueryRunner): Promise<void> {
 		await queryRunner.query(`
-            insert into measurement_type (id, measurement, "measurementUnit") values (1, 'Kilograms', 'kg');
-            insert into measurement_type (id, measurement, "measurementUnit") values (2, 'Grams', 'g');
-            insert into measurement_type (id, measurement, "measurementUnit") values (3, 'Onces', 'oz');
-            insert into measurement_type (id, measurement, "measurementUnit") values (4, 'Pound', 'lb');
-            insert into measurement_type (id, measurement, "measurementUnit") values (5, 'Liters', 'L');
-            insert into measurement_type (id, measurement, "measurementUnit") values (6, 'Cups', 'cups');
-            insert into measurement_type (id, measurement, "measurementUnit") values (7, 'Gallon', 'gal');
-            insert into measurement_type (id, measurement, "measurementUnit") values (8, 'Units', 'units');
+            insert into measurement_type (id, measurement, "measurementUnit") values (1, 'Kilograms', 'kg') on conflict do nothing;
+            insert into measurement_type (id, measurement, "measurementUnit") values (2, 'Grams', 'g') on conflict do nothing;
+            insert into measurement_type (id, measurement, "measurementUnit") values (3, 'Onces', 'oz') on conflict do nothing;
+            insert into measurement_type (id, measurement, "measurementUnit") values (4, 'Pound', 'lb') on conflict do nothing;
+            insert into measurement_type (id, measurement, "measurementUnit") values (5, 'Liters', 'L') on conflict do nothing;
+            insert into measurement_type (id, measurement, "measurementUnit") values (6, 'Cups', 'cups') on conflict do nothing;
+            insert into measurement_type (id, measurement, "measurementUnit") values (7, 'Gallon', 'gal') on conflict do nothing;
+            insert into measurement_type (id, measurement, "measurementUnit") values (8, 'Units', 'units') on conflict do nothing;
         `);
 	}
 
 	public async down(queryRunner: QueryRunner): Promise<void> {
-		await queryRunner.query(`
-            delete from measurement_type where id = 1;
-            delete from measurement_type where id = 2;
-            delete from measurement_type where id = 3;
-            delete from measurement_type where id = 4;
-            delete from measurement_type where id = 5;
-            delete from measurement_type where id = 6;
-            delete from measurement_type where id = 7;
-            delete from measurement_type where id = 8;
-            delete from measurement_type where id = 9;
-        `);
+		await queryRunner.query(``);
 	}
 }
